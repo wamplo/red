bf097474550053162d60b1569df2be9e629d164d
Adam Ramadhan <adamramadhan@live.com>
2011-09-26 01:40:03 +0700

    hermes dah balas, tinggal implementasi

--- engine/scripts/email/README.markdown
+++ engine/scripts/email/README.markdown
@@ -1,16 +1,5 @@
-PIPE ALL EMAIL TO HERE MASIH BERSAMA HERMES flash.php ( lib )
-	
-	#!/usr/bin/php –q
-	<?
-	/* Read the message from STDIN */
-	$fd = fopen("php://stdin", "r"); 
-	$email = ""; // This will be the variable holding the data.
-	while (!feof($fd)) {
-	$email .= fread($fd, 1024);
-	}
-	fclose($fd);
-	/* Saves the data into a file */
-	$fdw = fopen("mail.txt", "w+");
-	fwrite($fdw, $email);
-	fclose($fdw);
-	/* Script End */
+# PIPE ALL EMAIL TO HERE MASIH BERSAMA HERMES THE MESSENGER & flash.php ( lib )
+
+### SET PIPE TO PROGRAM CPANEL > MAIL > DEFAULT
+
+	public_html/engine/scripts/email/email.php
--- engine/scripts/email/email.php
+++ engine/scripts/email/email.php
@@ -1,12 +1,59 @@
 #!/usr/local/bin/php -q
 <?php
+
+ini_set("display_errors", "1");
+error_reporting(E_ALL);
+
 // read in email from stdin
 $fd = fopen("php://stdin", "r");
 $email = "";
 while (!feof($fd)) {
-$email .= fread($fd, 1024);
+    $email .= fread($fd, 1024);
 }
+
 fclose($fd);
- 
+
+// handle email
+$lines = explode("\n", $email);
+
+// empty vars
+$from = "";
+$subject = "";
+$headers = "";
+$message = "";
+$splittingheaders = true;
+for ($i=0; $i < count($lines); $i++) {
+    if ($splittingheaders) {
+        // this is a header
+        $headers .= $lines[$i]."\n";
+
+        // look out for special headers
+        if (preg_match("/^Subject: (.*)/", $lines[$i], $matches)) {
+            $subject = $matches[1];
+        }
+        if (preg_match("/^From: (.*)/", $lines[$i], $matches)) {
+            $from = $matches[1];
+        }
+        if (preg_match("/^To: (.*)/", $lines[$i], $matches)) {
+            $to = $matches[1];
+        }
+    } else {
+        // not a header, but message
+        $message .= $lines[$i]."\n";
+    }
+
+    if (trim($lines[$i])=="") {
+        // empty line, header section has ended
+        $splittingheaders = false;
+    }
+}
+
+$fheaders  = 'From: Netcoid <hermes-the-messenger@netcoid.com>;charset=UTF-8;Content-Type: text/html';
+
+$fmessage = $message;
+$fsubject = 'Re: ' . $subject;
+$to = $from;
 //send us the email to make sure it worked
-mail('rama@networks.co.id',$email");
+// check xss dan utf 8 cpanel kalo edit
+mail($to, $fsubject, $fmessage, $fheaders);
+?>
