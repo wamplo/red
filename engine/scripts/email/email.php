2ef5ee7d5c7cebbe6fc4b7d160d4bc9413a5d410
Adam Ramadhan <adamramadhan@live.com>
2011-09-26 00:48:13 +0700

    test hermes untuk ngepipe semua email

--- engine/scripts/email/email.php
+++ engine/scripts/email/email.php
@@ -1,6 +1,6 @@
-#!/usr/bin/php
+#!/usr/local/bin/php -q
 <?php
-// read in email from stdin
+// check atasnya penting! whm cpanel root kaya diatas
 $fd = fopen("php://stdin", "r");
 $email = "";
 while (!feof($fd)) {
@@ -8,6 +8,43 @@ $email .= fread($fd, 1024);
 }
 fclose($fd);
  
+// PARSE PARTS
+$mime=new mime_parser_class;
+$mime->ignore_syntax_errors = 1;
+$parameters=array(
+	'Data'=>$email,
+);
+
+$mime->Decode($parameters, $decoded);
+$fromName = $decoded[0]['ExtractedAddresses']['from:'][0]['name'];
+$fromEmail = $decoded[0]['ExtractedAddresses']['from:'][0]['address'];
+$toEmail = $decoded[0]['ExtractedAddresses']['to:'][0]['address'];
+$toName = $decoded[0]['ExtractedAddresses']['to:'][0]['name'];
+$subject = $decoded[0]['Headers']['subject:'];
+$removeChars = array('<','>');
+$messageID = str_replace($removeChars,'',$decoded[0]['Headers']['message-id:']);
+$replyToID = str_replace($removeChars,'',$decoded[0]['Headers']['in-reply-to:']);
+//get the message body
+if(substr($decoded[0]['Headers']['content-type:'],0,strlen('text/plain')) == 'text/plain' && isset($decoded[0]['Body'])){
+
+	$body = $decoded[0]['Body'];
+
+} elseif(substr($decoded[0]['Parts'][0]['Headers']['content-type:'],0,strlen('text/plain')) == 'text/plain' && isset($decoded[0]['Parts'][0]['Body'])) {
+
+	$body = $decoded[0]['Parts'][0]['Body'];
+
+} elseif(substr($decoded[0]['Parts'][0]['Parts'][0]['Headers']['content-type:'],0,strlen('text/plain')) == 'text/plain' && isset($decoded[0]['Parts'][0]['Parts'][0]['Body'])) {
+
+	$body = $decoded[0]['Parts'][0]['Parts'][0]['Body'];
+
+}
+
 //send us the email to make sure it worked
-mail('rama@networks.co.id','someone sent us an email at pipe@damnsemicolon.com',"Here is the the full email:\n\n$email");
-?>
+$to      = 'rama@networks.co.id';
+$subject = $subject;
+$message = $body;
+$headers = 'From: '. $toEmail . "\r\n" .
+    'Reply-To: '. $toEmail . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
