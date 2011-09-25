42ef4e4447dd15ac632610bdbd1d1493fce11f7b
Adam Ramadhan <adamramadhan@live.com>
2011-09-26 00:32:16 +0700

    test hermes untuk ngepipe semua email

--- engine/scripts/email/email.php
+++ engine/scripts/email/email.php
@@ -1,15 +1,13 @@
-#!/usr/local/lib/php –q
+#!/usr/bin/php
 <?php
- 
-// read from stdin
+// read in email from stdin
 $fd = fopen("php://stdin", "r");
 $email = "";
-while (!feof($fd))
-{
-	$email .= fread($fd, 1024);
+while (!feof($fd)) {
+$email .= fread($fd, 1024);
 }
 fclose($fd);
  
-mail('rama@networks.co.id','From my email pipe!','"' . $email . '"');
- 
+//send us the email to make sure it worked
+mail('rama@networks.co.id','someone sent us an email at pipe@damnsemicolon.com',"Here is the the full email:\n\n$email");
 ?>
